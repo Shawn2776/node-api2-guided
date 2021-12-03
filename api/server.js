2b66fd6683008a6fefb4222009b1beb99aa88585
Dave Kidd <dave@davekidd.net>
2021-12-02 19:06:52 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 // BREAK UP THIS MONOLITHIC FILE USING ROUTES
 // BREAK UP THIS MONOLITHIC FILE USING ROUTES
 const express = require('express');
-
+const adopterRouter = require("./adopters/adopters-router.js")
 const server = express();
 
 server.use(express.json());
