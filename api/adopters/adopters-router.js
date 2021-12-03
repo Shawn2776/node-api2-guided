253841386141921010ca88d309347f7f51f2cb20
Dave Kidd <dave@davekidd.net>
2021-12-02 19:08:30 -0700

    wip

--- api/adopters/adopters-router.js
+++ api/adopters/adopters-router.js
@@ -1,4 +1,6 @@
 const Adopter = require('./adopters-model');
+const express = require("express")
+const router = express.Router()
 // ADOPTERS ENDPOINTS
 // ADOPTERS ENDPOINTS
 // ADOPTERS ENDPOINTS
